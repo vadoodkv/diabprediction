797d925d2d3f08cfb303f202a1348dd3d4ee3aed
vadoodkv <vadoodkv@gmail.com>
2025-10-06 15:20:25 +0530

    Update views.py

--- diab/views.py
+++ diab/views.py
@@ -64,7 +64,7 @@ def predict(request):
 
 
 def result(request):
-    data = pd.read_csv(r"diabetes.csv")
+    data = pd.read_csv("diabetes.csv")
     
     x = data.drop('Outcome',axis=1)
     y = data['Outcome']
